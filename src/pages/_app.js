38d2c4f1e2ce631978d95a499e8885d3d9c88f77
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-10-06 03:56:32 +0700

    uninstall bootstrap

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,12 @@
 import "@/styles/globals.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { useEffect } from "react";
 import { RecoilRoot } from "recoil";
 import RecoilNexus from "recoil-nexus";
 
-const MyApp = ({ Component, pageProps }) => {
-  useEffect(() => {
-    import("bootstrap/dist/js/bootstrap");
-  }, []);
-
-  return (
-    <RecoilRoot>
-      <RecoilNexus />
-      <Component {...pageProps} />
-    </RecoilRoot>
-  );
-};
+const MyApp = ({ Component, pageProps }) => (
+  <RecoilRoot>
+    <RecoilNexus />
+    <Component {...pageProps} />
+  </RecoilRoot>
+);
 
 export default MyApp;
