a3c6319f2ab92f2e7f154ce5b11309628f86dd0a
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-10-08 22:24:31 +0700

    memperbaiki code untuk animasi loading

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,9 +9,7 @@ const MyApp = ({ Component, pageProps }) => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2500);
+    setTimeout(() => setIsLoading(false), 2500);
   }, []);
 
   return (
