fbcfc7fbfa5b166f70c88315718dc9d795cb00f7
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-10-08 22:23:01 +0700

    menambahkan animasi loading

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,32 @@
 import "@/styles/globals.css";
 import CssBaseline from "@mui/material/CssBaseline";
+import { useEffect } from "react";
+import { useState } from "react";
 import { RecoilRoot } from "recoil";
 import RecoilNexus from "recoil-nexus";
 
-const MyApp = ({ Component, pageProps }) => (
-  <RecoilRoot>
-    <CssBaseline />
-    <RecoilNexus />
-    <Component {...pageProps} />
-  </RecoilRoot>
-);
+const MyApp = ({ Component, pageProps }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2500);
+  }, []);
+
+  return (
+    <RecoilRoot>
+      <CssBaseline />
+      <RecoilNexus />
+      {isLoading ? (
+        <div className="min-h-screen flex justify-center items-center">
+          <h1>Loading...</h1>
+        </div>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </RecoilRoot>
+  );
+};
 
 export default MyApp;
