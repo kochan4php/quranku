8a1f9e953a33625c453b5aab3ce39e8b2099b597
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-10-11 17:03:10 +0700

    menampilkan per ayat

--- src/pages/surah/[surah].js
+++ src/pages/surah/[surah].js
@@ -3,6 +3,7 @@ import Layouts from "@/layouts";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
 export const getStaticPaths = async () => {
   const res = await fetch(SURAH);
@@ -37,13 +38,50 @@ const ReadSurah = ({ data }) => {
 
   return (
     <Layouts title={data?.name?.transliteration?.id}>
-      <Card variant="outlined">
-        <CardContent sx={{ pb: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
           <Typography variant="h4" align="center">
             {data?.name?.transliteration?.id}
           </Typography>
-        </CardContent>
-      </Card>
+        </Grid>
+        {data?.preBismillah && (
+          <Grid item xs={12}>
+            <Card variant="outlined">
+              <CardContent sx={{ pb: 1 }}>
+                <Typography align="center" variant="h4">
+                  {data?.preBismillah?.text?.arab}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        )}
+        {data?.verses?.map((item, index) => (
+          <Grid item xs={12} key={index}>
+            <Card variant="outlined">
+              <CardContent sx={{ pb: 1 }}>
+                <div
+                  className="flex justify-between items-center"
+                  style={{ gap: 10 }}
+                >
+                  <Typography align="left" variant="h5">
+                    {item?.number?.inSurah}.
+                  </Typography>
+                  <Typography align="right" variant="h4">
+                    {item?.text?.arab}
+                  </Typography>
+                </div>
+                <Typography
+                  align="center"
+                  color="text.secondary"
+                  sx={{ mt: 3 }}
+                >
+                  {item?.translation?.id}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </Layouts>
   );
 };
