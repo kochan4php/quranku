9f3d9909310303bd5f68a06feec34254b72f83ec
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-10-11 23:00:05 +0700

    perbaikan kecil

--- src/pages/surah/[surah].js
+++ src/pages/surah/[surah].js
@@ -1,9 +1,11 @@
 import { SURAH } from "@/config";
 import Layouts from "@/layouts";
 import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
 
 export const getServerSideProps = async (context) => {
   const surah = context.params.surah;
@@ -21,8 +23,8 @@ const ReadSurah = ({ data }) => (
   <Layouts title={data?.name?.transliteration?.id}>
     <Grid container spacing={2}>
       <Grid item xs={12}>
-        <Typography variant="h4" align="center">
-          {data?.name?.transliteration?.id}
+        <Typography variant="h5" align="center">
+          Surah {data?.name?.transliteration?.id}
         </Typography>
       </Grid>
       {data?.preBismillah && (
@@ -42,16 +44,20 @@ const ReadSurah = ({ data }) => (
       {data?.verses?.map((item, index) => (
         <Grid item xs={12} key={index}>
           <Card variant="outlined">
-            <CardContent sx={{ pb: 1 }}>
-              <div className="flex justify-between" style={{ gap: 4 }}>
-                <Typography align="left" variant="h6" sx={{ mt: 0.5 }}>
-                  {item?.number?.inSurah}.
+            <CardHeader
+              sx={{ p: 1 }}
+              title={
+                <Typography align="center" variant="body1">
+                  Ayat {item?.number?.inSurah}
                 </Typography>
-                <Typography align="right" sx={{ fontSize: "2.1rem" }}>
-                  {item?.text?.arab}
-                </Typography>
-              </div>
-              <Typography color="text.secondary" sx={{ mt: 3 }}>
+              }
+            />
+            <Divider />
+            <CardContent>
+              <Typography align="right" sx={{ fontSize: "2.1rem" }}>
+                {item?.text?.arab}
+              </Typography>
+              <Typography color="text.secondary" sx={{ mt: 3.5 }}>
                 {item?.translation?.id}
               </Typography>
             </CardContent>
