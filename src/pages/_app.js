945b6a6ecfbaa57ba37a9e594b8b9f7ae30aa433
kochan4php <aprodeosubarno@gmail.com>
2022-10-12 09:45:33 +0700

    menambahkan progress bar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,23 +1,12 @@
 import "@/styles/globals.css";
 import CssBaseline from "@mui/material/CssBaseline";
+import NextNProgress from "nextjs-progressbar";
 import { RecoilRoot } from "recoil";
 import RecoilNexus from "recoil-nexus";
-import Router from "next/router";
-import nProgress from "nprogress";
-import Head from "next/head";
-
-Router.onRouteChangeStart = (url) => nProgress.start();
-Router.onRouteChangeComplete = () => nProgress.done();
-Router.onRouteChangeError = () => nProgress.done();
 
 const MyApp = ({ Component, pageProps }) => (
   <RecoilRoot>
-    <Head>
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
-      />
-    </Head>
+    <NextNProgress color="#FFC500" height={3} />
     <CssBaseline />
     <RecoilNexus />
     <Component {...pageProps} />
